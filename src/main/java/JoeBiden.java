b0edd50f16f28ef4b2f8af637953b6cd0f961f92
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-23 16:39:53 +0800

    Add list command

--- src/main/java/JoeBiden.java
+++ src/main/java/JoeBiden.java
@@ -1,28 +1,45 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class JoeBiden {
 
     public static final String NAME = "Joe Biden";
+
+    private static final ArrayList<String> list = new ArrayList<>();
+
+
     private static final String LINE =
             "____________________________________________________________";
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-
         System.out.println(getWelcomeBanner());
-
         while (true) {
+
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("bye")) {
-                break;
+            String[] parts = input.split(" ", 2);
+            String command = parts[0].toLowerCase();
+            switch(command){
+                case "bye":
+                    System.out.println(getGoodbyeBanner());
+                    scanner.close();
+                    return;
+                case "list":
+                    for (int i = 0; i < list.size(); i++) {
+                        System.out.println((i + 1) + ". " + list.get(i));
+                    }
+                    break;
+                default:
+                    if(parts.length > 1){
+                        list.add(parts[1]);
+                        echo("added: " + parts[1]);
+                    }
+                    break;
             }
 
-            echo(input);
+
         }
-
-        System.out.println(getGoodbyeBanner());
-        scanner.close();
-
     }
 
     public static void echo(String input) {
@@ -30,6 +47,8 @@ public class JoeBiden {
         System.out.println(input);
         System.out.println(LINE);
     }
+
+
 
     public static String getWelcomeBanner() {
         return LINE + "\n"
