dab5d4cc719956e753df483822923a287d7c12f5
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-23 17:13:05 +0800

    Add list and Task class completion

--- src/main/java/JoeBiden.java
+++ src/main/java/JoeBiden.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class JoeBiden {
 
     public static final String NAME = "Joe Biden";
 
-    private static final ArrayList<String> list = new ArrayList<>();
+    private static final ArrayList<Task> list = new ArrayList<>();
 
 
     private static final String LINE =
@@ -26,13 +27,36 @@ public class JoeBiden {
                     scanner.close();
                     return;
                 case "list":
+                    System.out.println("Here are the tasks in your list:\n");
                     for (int i = 0; i < list.size(); i++) {
-                        System.out.println((i + 1) + ". " + list.get(i));
+                        System.out.println((i + 1) + ". " + list.get(i).toString());
+                    }
+                    System.out.println(LINE);
+                    break;
+                case "mark":
+                    if(parts.length > 1) {
+                        int number = Integer.parseInt(parts[1]);
+                        Task task = list.get(number - 1);
+
+                        System.out.println(LINE + "\n"
+                                + task.markDone()
+                                + LINE);
                     }
                     break;
+                case "unmark":
+                    if(parts.length > 1) {
+                        int number = Integer.parseInt(parts[1]);
+                        Task task = list.get(number - 1);
+                        System.out.println(LINE + "\n"
+                                + task.unmark()
+                                + LINE);
+                    }
+                    break;
+
                 default:
                     if(parts.length > 1){
-                        list.add(parts[1]);
+                        Task task = new Task(parts[1]);
+                        list.add(task);
                         echo("added: " + parts[1]);
                     }
                     break;
@@ -47,7 +71,6 @@ public class JoeBiden {
         System.out.println(input);
         System.out.println(LINE);
     }
-
 
 
     public static String getWelcomeBanner() {
---
+++ src/main/java/Task.java
@@ -0,0 +1,24 @@
+public class Task {
+    private final String name;
+    private boolean isDone;
+    public Task(String name) {
+        this.name = name;
+        this.isDone = false;
+    }
+    public String markDone() {
+        isDone = true;
+        return "Nice! I've marked this task as done:\n"
+                + this.toString()
+                + "\n";
+    }
+    public String unmark() {
+        isDone = false;
+        return "OK, I've marked this task as not done yet:\n"
+                + this.toString()
+                + "\n";
+    }
+    @Override
+    public String toString() {
+        return (isDone ? "[X] " : "[ ] ") + name;
+    }
+}
