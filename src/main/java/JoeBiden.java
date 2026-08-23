6ccbf92ea911e93034eb3e6ef4d20d13e2817c46
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-23 16:15:21 +0800

    Add echo command

--- src/main/java/JoeBiden.java
+++ src/main/java/JoeBiden.java
@@ -1,20 +1,45 @@
+import java.util.Scanner;
 public class JoeBiden {
 
     public static final String NAME = "Joe Biden";
+    private static final String LINE =
+            "____________________________________________________________";
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println(getWelcomeBanner());
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            echo(input);
+        }
+
         System.out.println(getGoodbyeBanner());
+        scanner.close();
+
     }
+
+    public static void echo(String input) {
+        System.out.println(LINE);
+        System.out.println(input);
+        System.out.println(LINE);
+    }
+
     public static String getWelcomeBanner() {
-        return "____________________________________________________________\n"
+        return LINE + "\n"
                 + "Hello! I'm " + NAME + ".\n"
                 + "What can I do for you?\n"
-                + "____________________________________________________________";
+                + LINE;
     }
 
     public static String getGoodbyeBanner() {
         return "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + LINE;
     }
 }
