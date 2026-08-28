418e579cf65c11f3408f0e9cf9eec49dab2674e9
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-28 18:59:59 +0800

    Extract UI handling

---
+++ src/main/java/Ui.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ui {
+
+    private final Scanner scanner;
+
+    public Ui() {
+        scanner = new Scanner(System.in);
+    }
+
+    public String readInput() {
+        return scanner.nextLine();
+    }
+
+    public void showMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void close() {
+        scanner.close();
+    }
+}
