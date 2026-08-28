799fe3d86138e49866a34b8fc653cac16427db0f
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-28 19:14:41 +0800

    Added Parser ErrorHandling

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,4 +1,10 @@
 public class Parser {
+
+    public static String getCommand(String input) {
+        String[] parts = input.split(" ", 2);
+        return parts[0].toLowerCase();
+    }
+
     public static String getArguments(String input) {
         String[] parts = input.split(" ", 2);
 
@@ -8,8 +14,22 @@ public class Parser {
 
         return parts[1];
     }
-    public static String getCommand(String input) {
-        String[] parts = input.split(" ", 2);
-        return parts[0].toLowerCase();
+
+    public static int getTaskNumber(String input) throws JoeBidenException {
+        String argument = getArguments(input);
+
+        if (argument.isBlank()) {
+            throw new JoeBidenException(
+                    "Please specify a task number."
+            );
+        }
+
+        try {
+            return Integer.parseInt(argument);
+        } catch (NumberFormatException e) {
+            throw new JoeBidenException(
+                    "Task number must be a number."
+            );
+        }
     }
 }
