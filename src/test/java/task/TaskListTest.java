0aa3e75e896ebd6f1cda7943257747632df94df4
yixuan <146533850+tanyxuan@users.noreply.github.com>
2026-08-28 22:10:08 +0800

    Add JUnit tests for Parser and TaskList

---
+++ src/test/java/task/TaskListTest.java
@@ -0,0 +1,51 @@
+package task;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import exception.JoeBidenException;
+
+public class TaskListTest {
+
+    @Test
+    public void deleteTask_validNumber_removesCorrectTask()
+            throws JoeBidenException {
+
+        TaskList tasks = new TaskList();
+
+        tasks.addTask(new Todo("read book"));
+        tasks.addTask(new Todo("sleep"));
+
+        Task removed = tasks.deleteTask(1);
+
+        assertEquals("read book", removed.getName());
+        assertEquals(1, tasks.size());
+        assertEquals("sleep", tasks.getTask(1).getName());
+    }
+
+    @Test
+    public void deleteTask_invalidNumber_throwsException() {
+
+        TaskList tasks = new TaskList();
+        tasks.addTask(new Todo("read book"));
+
+        assertThrows(
+                JoeBidenException.class,
+                () -> tasks.deleteTask(5)
+        );
+    }
+
+    @Test
+    public void markTask_validNumber_marksTaskDone()
+            throws JoeBidenException {
+
+        TaskList tasks = new TaskList();
+        tasks.addTask(new Todo("read book"));
+
+        tasks.markTask(1);
+
+        assertEquals(true, tasks.getTask(1).isDone());
+    }
+}
